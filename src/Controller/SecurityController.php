e96aeceeb0dff172aa535af32b9cc9d2c007d071
Zeyoman <alex10pump@gmail.com>
2025-01-02 10:29:12 +0100

    rectification publicAccess

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -13,7 +13,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
-            return $this->redirectToRoute('favoris');
+            return $this->redirectToRoute('Homepage');
         }
 
         // get the login error if there is one
